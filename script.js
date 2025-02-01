49ea88f5f9e7290325a16d87dd3d9a30cbec57a9
Xiao-Ming Wu <84899966+DravenALG@users.noreply.github.com>
2025-02-02 01:13:54 +0800

    update the page with full list of published papers

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
         img.src = 'images/myself.jpg';
     });
 });
+
+
+function showFullList() {
+    document.getElementById('selectedPublications').style.display = 'none';
+    document.getElementById('fullPublications').style.display = 'table-row-group';
+    document.getElementById('showFullListBtn').style.display = 'none';
+    document.getElementById('showSelectedListBtn').style.display = 'inline';
+  }
+  
+  function showSelectedList() {
+    document.getElementById('selectedPublications').style.display = 'table-row-group';
+    document.getElementById('fullPublications').style.display = 'none';
+    document.getElementById('showFullListBtn').style.display = 'inline';
+    document.getElementById('showSelectedListBtn').style.display = 'none';
+  }
